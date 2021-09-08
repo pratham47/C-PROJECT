d4a299e06889e805ad59160cc9f9d3f057b1f8fa
pratham47 <57667615+pratham47@users.noreply.github.com>
2021-09-08 18:56:54 +0530

    Update fib_search.c

--- Fibonacci Search/fib_search.c
+++ Fibonacci Search/fib_search.c
@@ -27,31 +27,31 @@ int fib_search(void **arr, int n, void *key, int (*cmp)(void *,void * ))
 	int offset = -1; // Marking eliminated range from front of the array
 
 	while (m > 1)
-    {
+        {
 		int i = min(offset + m_2, n - 1); // To check if offset + m_2 fibonacci number range is valid to search the key
 
 		if((*cmp)(arr[i],key) == -1) // If key is greater than the value at index m-2, we cut the subarray array from offset to i index
-        {
+                {
 			m = m_1;
 			m_1 = m_2;
 			m_2 = m - m_1;
 			offset = i;
 		}
 		else if((*cmp)(arr[i],key) == 1) // If key is smaller than the value at index m-2, we cut the subarray after i+1 (i.e m_2 + 1)
-        {
+                {
 			m = m_2;
 			m_1 = m_1 - m_2;
 			m_2 = m - m_1;
 		}
 		else
-        {
-            return i; // returning element if found
-        }
-	}
+                {
+                       return i; // returning element if found
+                }
+	 }
 
-	if(m_1 && ((*cmp)(arr[offset+1],key) == 0)) // comparison of last element of array for the given key
+    if(m_1 && ((*cmp)(arr[offset+1],key) == 0)) // comparison of last element of array for the given key
     {
-        return offset + 1;
+      return offset + 1;
     }
 	return -1;
 }
